baeada8f7d34c2996f331d35f3086a7e4e77e269
andersonqrz <anderson.afonso@uniriotec.br>
2019-09-01 15:57:08 -0300

    basemaster

--- testando/src/testando/fome.java
+++ testando/src/testando/fome.java
@@ -4,8 +4,7 @@ public class fome {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello branch");
-		System.out.println("Hello water");
+		
 	}
 
 }
